5b3103977811e4d4677a5c1474f671834993b015
AIM <adeshinaibrahim571@gmail.com>
2026-01-22 15:02:22 +0100

    added vercel.json for fixing routing problem

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,6 +14,7 @@ import path from "path"
 import morgan from "morgan";
 import { isAdmin } from "./middleware/isAdmin.js";
 import auth from "./middleware/authHandler.js";
+import { uptime } from "process";
 
 const app = express();
 
