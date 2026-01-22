f6eab7ce8e2b5c2e739f8bc48ed9567cd61957b1
AIM <131113459+Mubarak-Ade@users.noreply.github.com>
2026-01-22 15:25:00 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,17 +4,14 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from "./App.js";
-import {HashRouter} from "react-router";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
-    </HashRouter>
   </StrictMode>
 );
